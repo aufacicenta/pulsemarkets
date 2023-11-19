7180fd1624bf857e7720f4412281b5b7f2562f6e
Udit Takkar <udit222001@gmail.com>
2023-11-20 01:42:43 +0530

    feat: add score

--- app/src/pages/api/prompt-wars/preview.ts
+++ app/src/pages/api/prompt-wars/preview.ts
@@ -1,8 +1,11 @@
 import Replicate from "replicate";
 import { NextApiRequest, NextApiResponse } from "next";
+import Jimp from "jimp";
 
 import ipfs from "providers/ipfs";
 import logger from "providers/logger";
+import pulse from "providers/pulse";
+import { PromptWarsMarketContract } from "providers/near/contracts/prompt-wars/contract";
 
 const DEFAULT_IMG_DIMENSIONS = { width: 512, height: 512 };
 
@@ -10,8 +13,15 @@ export default async function Fn(request: NextApiRequest, response: NextApiRespo
   const { prompt, negative_prompt } = JSON.parse(request.body);
 
   try {
-    const model = "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf";
+    const marketId = await pulse.promptWars.getLatestMarketId();
 
+    const market = await PromptWarsMarketContract.loadFromGuestConnection(marketId!);
+
+    const { image_uri } = await market.get_market_data();
+    const sourceImgURL = ipfs.asHttpsURL(image_uri);
+    const sourceImg = await Jimp.read(sourceImgURL);
+
+    const model = "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf";
     const image_dimensions = `${DEFAULT_IMG_DIMENSIONS.width}x${DEFAULT_IMG_DIMENSIONS.height}`;
 
     const replicate = new Replicate({
@@ -26,8 +36,11 @@ export default async function Fn(request: NextApiRequest, response: NextApiRespo
 
     const [outputImgURL] = (await replicate.run(model, { input })) as Array<string>;
     const ipfsOutputImgURL = await ipfs.getFileAsIPFSUrl(outputImgURL);
+    const destImg = await Jimp.read(outputImgURL);
 
-    return response.status(200).json({ imageUrl: ipfsOutputImgURL });
+    const { percent } = Jimp.diff(sourceImg, destImg);
+
+    return response.status(200).json({ imageUrl: ipfsOutputImgURL, score: percent });
   } catch (error) {
     logger.error(error);
 
