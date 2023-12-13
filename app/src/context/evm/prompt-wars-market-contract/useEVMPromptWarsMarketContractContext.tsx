eea26519e7ceeaaef2601aa654d4c38bb3c19bf7
Gustavo Ibarra <gus@aufacicenta.com>
2023-12-13 09:30:13 -0600

    chore: updates EVM hook name

--- app/src/context/evm/prompt-wars-market-contract/useEVMPromptWarsMarketContractContext.tsx
+++ app/src/context/evm/prompt-wars-market-contract/useEVMPromptWarsMarketContractContext.tsx
@@ -2,11 +2,11 @@ import { useContext } from "react";
 
 import { PromptWarsMarketContractContext } from "./PromptWarsMarketContractContext";
 
-export const usePromptWarsMarketContractContext = () => {
+export const useEVMPromptWarsMarketContractContext = () => {
   const context = useContext(PromptWarsMarketContractContext);
 
   if (context === undefined) {
-    throw new Error("usePromptWarsMarketContractContext must be used within a PromptWarsMarketContractContext");
+    throw new Error("useEVMPromptWarsMarketContractContext must be used within a PromptWarsMarketContractContext");
   }
 
   return context;
