d01971a5b14ae4657d6a0c6dcd192b3995835455
Udit Takkar <udit222001@gmail.com>
2023-12-16 23:49:20 +0530

    feat: add other methods

--- app/src/context/evm/prompt-wars-market-contract/PromptWarsMarketContractContext.types.ts
+++ app/src/context/evm/prompt-wars-market-contract/PromptWarsMarketContractContext.types.ts
@@ -3,8 +3,49 @@ import { Signer, ethers } from "ethers";
 
 import { Market } from "providers/evm/contracts/prompt-wars";
 
+type AccountId = string;
+
 export type PromptWarsMarketContractContextControllerProps = {
+  marketId: AccountId;
   children: ReactNode;
+};
+
+export enum PromptWarsMarketContractStatus {
+  LOADING = "Loading",
+  OPEN = "Open",
+  REVEALING = "Revealing",
+  RESOLVING = "Resolving",
+  RESOLVED = "Resolved",
+  UNRESOLVED = "Unresolved",
+  CLOSED = "Closed",
+}
+
+export type PromptWarsMarketContractValues = {
+  market: Market.MarketDataStruct;
+  resolution: Market.ResolutionStruct;
+  fees: Market.FeesStruct;
+  management: Market.ManagementStruct;
+  collateralToken: Market.CollateralTokenStruct;
+  outcomeIds: Array<string>;
+  isResolved: boolean;
+  isOpen: boolean;
+  isOver: boolean;
+  isRevealWindowExpired: boolean;
+  isResolutionWindowExpired: boolean;
+  isExpiredUnresolved: boolean;
+
+  status: PromptWarsMarketContractStatus;
+};
+
+export type PromptWarsMarketContractContextContextActions = {
+  fetchMarketContractValues: { isLoading: boolean };
+  ftTransferCall: {
+    isLoading: boolean;
+    success: boolean;
+  };
+  create: {
+    isLoading: boolean;
+  };
 };
 
 export type PromptWarsMarketContractContextType = {
@@ -16,4 +57,12 @@ export type PromptWarsMarketContractContextType = {
   ) => Promise<Market>;
 
   connect: (address: string, provider: ethers.Signer | ethers.Provider) => Promise<Market>;
+
+  fetchMarketContractValues: () => Promise<void>;
+  sell: () => Promise<void>;
+  ftTransferCall: (prompt: string) => Promise<void>;
+  actions: PromptWarsMarketContractContextContextActions;
+  marketId: AccountId;
+  marketContractValues?: PromptWarsMarketContractValues;
+  create: () => Promise<void>;
 };
--- app/src/context/evm/prompt-wars-market-contract/PromptWarsMarketContractContextController.tsx
+++ app/src/context/evm/prompt-wars-market-contract/PromptWarsMarketContractContextController.tsx
@@ -1,13 +1,41 @@
-import React from "react";
-import { Signer, ethers } from "ethers";
+import React, { useState } from "react";
+import { Signer, ethers, BrowserProvider, JsonRpcSigner } from "ethers";
+import { type WalletClient, useWalletClient, useAccount } from "wagmi";
+import { useRouter } from "next/router";
 
 import { Market, Market__factory } from "providers/evm/contracts/prompt-wars";
+import { useRoutes } from "hooks/useRoutes/useRoutes";
+import { useToastContext } from "hooks/useToastContext/useToastContext";
+import { Typography } from "ui/typography/Typography";
 
 import { PromptWarsMarketContractContext } from "./PromptWarsMarketContractContext";
 import {
   PromptWarsMarketContractContextControllerProps,
   PromptWarsMarketContractContextType,
+  PromptWarsMarketContractStatus,
+  PromptWarsMarketContractValues,
+  PromptWarsMarketContractContextContextActions,
 } from "./PromptWarsMarketContractContext.types";
+
+export function walletClientToSigner(walletClient: WalletClient) {
+  const { account, chain, transport } = walletClient;
+  const network = {
+    chainId: chain.id,
+    name: chain.name,
+    ensAddress: chain.contracts?.ensRegistry?.address,
+  };
+  const provider = new BrowserProvider(transport, network);
+  const signer = new JsonRpcSigner(provider, account.address);
+
+  return signer;
+}
+
+/** Hook to convert a viem Wallet Client to an ethers.js Signer. */
+export function useEthersSigner({ chainId }: { chainId?: number } = {}) {
+  const { data: walletClient } = useWalletClient({ chainId });
+
+  return React.useMemo(() => (walletClient ? walletClientToSigner(walletClient) : undefined), [walletClient]);
+}
 
 const deploy = async (
   signer: Signer,
@@ -32,12 +60,317 @@ const connect = async (address: string, provider: ethers.Signer | ethers.Provide
   return contract;
 };
 
+const DEPLOYED_CONTRACT_ADDRESS = "0x00";
+
+const getMarketStatus = (values: PromptWarsMarketContractValues): PromptWarsMarketContractStatus => {
+  if (!values) {
+    return PromptWarsMarketContractStatus.LOADING;
+  }
+
+  if (values?.isOpen) {
+    return PromptWarsMarketContractStatus.OPEN;
+  }
+
+  if (values?.isOver && values.isResolved) {
+    return PromptWarsMarketContractStatus.RESOLVED;
+  }
+
+  if (values?.isOver && !values.isRevealWindowExpired) {
+    return PromptWarsMarketContractStatus.REVEALING;
+  }
+
+  if (values?.isOver && !values.isResolutionWindowExpired) {
+    return PromptWarsMarketContractStatus.RESOLVING;
+  }
+
+  if (values?.isOver && values.isExpiredUnresolved) {
+    return PromptWarsMarketContractStatus.UNRESOLVED;
+  }
+
+  return PromptWarsMarketContractStatus.CLOSED;
+};
+
+let marketContract: Market;
+
 export const PromptWarsMarketContractContextController = ({
+  marketId,
   children,
 }: PromptWarsMarketContractContextControllerProps) => {
+  const routes = useRoutes();
+  const router = useRouter();
+  const toast = useToastContext();
+  const signer = useEthersSigner();
+
+  const [marketContractValues, setMarketContractValues] = useState<PromptWarsMarketContractValues>();
+
+  const [actions, setActions] = useState<PromptWarsMarketContractContextContextActions>({
+    fetchMarketContractValues: {
+      isLoading: false,
+    },
+    ftTransferCall: {
+      success: false,
+      isLoading: false,
+    },
+    create: {
+      isLoading: false,
+    },
+  });
+
+  const { isConnected } = useAccount();
+
+  const assertWalletConnection = () => {
+    if (!isConnected) {
+      toast.trigger({
+        variant: "info",
+        withTimeout: true,
+        title: "Wallet is not connected",
+        children: <Typography.Text>Check your internet connection, your balance and try again.</Typography.Text>,
+      });
+
+      throw new Error("ERR_PROMPT_MARKET_CONTRACT_INVALID_WALLET_CONNECTION");
+    }
+  };
+
+  const fetchMarketContractValues = async () => {
+    setActions((prev) => ({
+      ...prev,
+      fetchMarketContractValues: {
+        isLoading: true,
+      },
+    }));
+
+    try {
+      // Wait 1 second to allow flags to change
+      setTimeout(async () => {
+        try {
+          if (!signer) throw new Error("not signed in");
+
+          const contract = await connect(DEPLOYED_CONTRACT_ADDRESS, signer);
+
+          const [
+            market,
+            resolution,
+            fees,
+            management,
+            collateralToken,
+            // outcomeIds,
+            // isResolved,
+            // isOpen,
+            // isOver,
+            // isRevealWindowExpired,
+            // isResolutionWindowExpired,
+            // isExpiredUnresolved,
+          ] = await Promise.all([
+            contract.get_market_data(),
+            contract.get_resolution_data(),
+            contract.get_fees_data(),
+            contract.get_management_data(),
+            contract.get_collateral_token_data(),
+            // contract.get_outcome_ids(),
+            // contract.is_resolved(),
+            // contract.is_open(),
+            // contract.is_over(),
+            // contract.is_reveal_window_expired(),
+            // contract.is_resolution_window_expired(),
+            // contract.is_expired_unresolved(),
+          ]);
+
+          const values: PromptWarsMarketContractValues = {
+            market,
+            resolution,
+            fees,
+            management,
+            collateralToken,
+            // TODO:
+            outcomeIds: [],
+            isResolved: false,
+            isOpen: false,
+            isOver: false,
+            isRevealWindowExpired: false,
+            isResolutionWindowExpired: false,
+            isExpiredUnresolved: false,
+            status: PromptWarsMarketContractStatus.LOADING,
+          };
+
+          const status = getMarketStatus(values);
+
+          values.status = status;
+
+          setMarketContractValues(values);
+        } catch (error) {
+          console.log(error);
+        }
+      }, 1000);
+    } catch {
+      toast.trigger({
+        variant: "error",
+        withTimeout: true,
+        title: "Failed to fetch market data",
+        children: <Typography.Text>Try refreshing the page, or check your internet connection.</Typography.Text>,
+      });
+    }
+
+    setActions((prev) => ({
+      ...prev,
+      fetchMarketContractValues: {
+        isLoading: false,
+      },
+    }));
+  };
+
+  // const getOutcomeToken = async (args: GetOutcomeTokenArgs) => {
+  //   try {
+  //     if (!signer) throw new Error("not signed in");
+
+  //     if (!marketContract) {
+  //       marketContract = await await connect(DEPLOYED_CONTRACT_ADDRESS, signer);
+  //     }
+
+  //     const outcomeToken = await marketContract.get_outcome_token(args);
+
+  //     return outcomeToken;
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+
+  //   return undefined;
+  // };
+
+  const sell = async () => {
+    try {
+      assertWalletConnection();
+
+      await marketContract.sell();
+
+      toast.trigger({
+        variant: "confirmation",
+        withTimeout: false,
+        title: "Success",
+        children: <Typography.Text>Check your new wallet balance.</Typography.Text>,
+      });
+    } catch {
+      toast.trigger({
+        variant: "error",
+        withTimeout: true,
+        title: "Failed to call sell method",
+        children: (
+          <Typography.Text>Check your internet connection, your wallet connection and try again.</Typography.Text>
+        ),
+      });
+    }
+  };
+
+  const ftTransferCall = async (prompt: string) => {
+    if (!marketContractValues) {
+      return;
+    }
+
+    try {
+      assertWalletConnection();
+
+      setActions((prev) => ({
+        ...prev,
+        ftTransferCall: {
+          ...prev.ftTransferCall,
+          isLoading: true,
+        },
+      }));
+
+      const amount = marketContractValues.fees.price.toString();
+      const msg = JSON.stringify({ CreateOutcomeTokenArgs: { prompt: JSON.stringify(prompt) } });
+
+      // TODO: What is fungible contract
+      // await FungibleTokenContract.ftTransferCall(
+      //   walletStateContext.context.wallet!,
+      //   marketContractValues.collateralToken.id!,
+      //   {
+      //     receiver_id: marketId,
+      //     amount,
+      //     msg,
+      //   },
+      // );
+
+      setActions((prev) => ({
+        ...prev,
+        ftTransferCall: {
+          ...prev.ftTransferCall,
+          isLoading: false,
+          success: true,
+        },
+      }));
+
+      toast.trigger({
+        variant: "confirmation",
+        withTimeout: true,
+        title: "Your prompt was successfully submitted",
+        children: <Typography.Text>{`Transferred USDT ${amount} to ${marketId}`}</Typography.Text>,
+      });
+
+      fetchMarketContractValues();
+    } catch {
+      toast.trigger({
+        variant: "error",
+        title: "Failed to make transfer call",
+        children: <Typography.Text>Check your internet connection, connect your wallet and try again.</Typography.Text>,
+      });
+
+      setActions((prev) => ({
+        ...prev,
+        ftTransferCall: {
+          ...prev.ftTransferCall,
+          isLoading: false,
+          success: true,
+        },
+      }));
+    }
+  };
+
+  const create = async () => {
+    setActions((prev) => ({
+      ...prev,
+      create: {
+        isLoading: true,
+      },
+    }));
+
+    try {
+      const response = await fetch(routes.api.promptWars.create());
+
+      if (!response.ok) {
+        throw new Error("ERR_USE_PROMPT_WARS_MARKET_CONTRACT_CREATE_FAILED");
+      }
+
+      router.push(routes.dashboard.promptWars.home());
+    } catch (error) {
+      console.log(error);
+
+      toast.trigger({
+        variant: "error",
+        withTimeout: false,
+        title: "Failed to create a new market",
+        children: <Typography.Text>The server must have run out of funds. Please try again later.</Typography.Text>,
+      });
+    }
+
+    setActions((prev) => ({
+      ...prev,
+      create: {
+        isLoading: false,
+      },
+    }));
+  };
+
   const props: PromptWarsMarketContractContextType = {
     deploy,
     connect,
+    fetchMarketContractValues,
+    sell,
+    ftTransferCall,
+    actions,
+    marketId,
+    marketContractValues,
+    create,
+    // getOutcomeToken,
   };
 
   return <PromptWarsMarketContractContext.Provider value={props}>{children}</PromptWarsMarketContractContext.Provider>;
